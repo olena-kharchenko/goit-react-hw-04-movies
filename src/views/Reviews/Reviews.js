5d9772ba08739f0e294b39911ee19a6834cfb340
olena-kharchenko <harchenko.es1990@gmail.com>
2020-12-24 12:12:13 +0200

    add review

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -1,3 +1,28 @@
-export default function Reviews() {
-  return <p>Reviews</p>;
+import { useState, useEffect } from 'react';
+import { fetchMovieReviews, POSTER_URL } from '../../services/movies-api';
+import s from './Reviews.module.css';
+
+export default function Reviews({ movieId }) {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetchMovieReviews(movieId).then(request => setReviews(request.results));
+  }, [movieId]);
+
+  return (
+    <div className={s.wrapper}>
+      {reviews && (
+        <>
+          <ul className={s.list}>
+            {reviews.map((item, index) => (
+              <li key={index} className={s.item}>
+                <p className={s.title}> {item.author}</p>
+                <p> {item.content}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
